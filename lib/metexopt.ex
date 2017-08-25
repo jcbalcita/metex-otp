34e89c2dc8dd2a27deba1d377b81cd292bc29c9b
jbalcita <jbalcita@cj.com>
2017-08-25 09:48:27 -0700

    initial project state

---
+++ lib/metexopt.ex
@@ -0,0 +1,18 @@
+defmodule Metexopt do
+  @moduledoc """
+  Documentation for Metexopt.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Metexopt.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/metexopt_test.exs
@@ -0,0 +1,8 @@
+defmodule MetexoptTest do
+  use ExUnit.Case
+  doctest Metexopt
+
+  test "greets the world" do
+    assert Metexopt.hello() == :world
+  end
+end
