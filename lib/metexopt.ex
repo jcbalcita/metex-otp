688c6c828480a44365ab30b7266217e54005db40
jbalcita <jbalcita@cj.com>
2017-08-26 08:18:07 -0700

    remove default module boilerplate

--- lib/metexopt.ex
+++ lib/metexopt.ex
@@ -2,17 +2,4 @@ defmodule Metexopt do
   @moduledoc """
   Documentation for Metexopt.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Metexopt.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
